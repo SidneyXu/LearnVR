fdc983793727e0a474105b3952d56340e676c076
SidneyXu <siriuseddy@gmail.com>
2016-05-12 08:30:32 +0800

    foobar

---
+++ app/src/main/java/com/bookislife/firstvr/VRHuntActivity.java
@@ -0,0 +1,196 @@
+package com.bookislife.firstvr;
+
+import android.opengl.GLES20;
+import android.os.Vibrator;
+import android.util.Log;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+import com.google.vrtoolkit.cardboard.audio.CardboardAudioEngine;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+/**
+ * Created by SidneyXu on 2016/05/12.
+ */
+public class VRHuntActivity
+        extends CardboardActivity implements CardboardView.StereoRenderer {
+    private static final String TAG = VRHuntActivity.class.getSimpleName();
+
+    private static final float Z_NEAR = 0.1f;
+    private static final float Z_FAR = 100.0f;
+
+    private static final float CAMERA_Z = 0.01f;
+    private static final float TIME_DELTA = 0.3f;
+
+    private static final float YAW_LIMIT = 0.12f;
+    private static final float PITCH_LIMIT = 0.12f;
+
+    private static final int COORDS_PER_VERTEX = 3;
+
+    // We keep the light always position just above the user.
+    private static final float[] LIGHT_POS_IN_WORLD_SPACE = new float[]{0.0f, 2.0f, 0.0f, 1.0f};
+
+    private static final float MIN_MODEL_DISTANCE = 3.0f;
+    private static final float MAX_MODEL_DISTANCE = 7.0f;
+
+    private static final String SOUND_FILE = "cube_sound.wav";
+
+    private final float[] lightPosInEyeSpace = new float[4];
+
+    private FloatBuffer floorVertices;
+    private FloatBuffer floorColors;
+    private FloatBuffer floorNormals;
+
+    private FloatBuffer cubeVertices;
+    private FloatBuffer cubeColors;
+    private FloatBuffer cubeFoundColors;
+    private FloatBuffer cubeNormals;
+
+    private int cubeProgram;
+    private int floorProgram;
+
+    private int cubePositionParam;
+    private int cubeNormalParam;
+    private int cubeColorParam;
+    private int cubeModelParam;
+    private int cubeModelViewParam;
+    private int cubeModelViewProjectionParam;
+    private int cubeLightPosParam;
+
+    private int floorPositionParam;
+    private int floorNormalParam;
+    private int floorColorParam;
+    private int floorModelParam;
+    private int floorModelViewParam;
+    private int floorModelViewProjectionParam;
+    private int floorLightPosParam;
+
+    private float[] modelCube;
+    private float[] camera;
+    private float[] view;
+    private float[] headView;
+    private float[] modelViewProjection;
+    private float[] modelView;
+    private float[] modelFloor;
+
+    private float[] modelPosition;
+    private float[] headRotation;
+
+    private float objectDistance = MAX_MODEL_DISTANCE / 2.0f;
+    private float floorDepth = 20f;
+
+    private Vibrator vibrator;
+
+    private CardboardAudioEngine cardboardAudioEngine;
+    private volatile int soundId = CardboardAudioEngine.INVALID_ID;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cardboardAudioEngine.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cardboardAudioEngine.resume();
+    }
+
+    //处理输入
+    @Override
+    public void onCardboardTrigger() {
+        super.onCardboardTrigger();
+    }
+
+    //每次进行渲染时调用,用于更新模型
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    //针对眼睛参数不同的每只眼睛进行调用
+    @Override
+    public void onDrawEye(Eye eye) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+        Log.i(TAG, "onSurfaceChanged()");
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+        Log.i(TAG, "onSurfaceCreated()");
+
+        // Background
+        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 0.5f);
+
+        // Init Cube
+        ByteBuffer bbVertices = ByteBuffer.allocateDirect(WorldLayoutData.CUBE_COORDS.length * 4);
+        bbVertices.order(ByteOrder.nativeOrder());
+        cubeVertices = bbVertices.asFloatBuffer();
+        cubeVertices.put(WorldLayoutData.CUBE_COORDS);
+        cubeVertices.position(0);
+
+        ByteBuffer bbColors = ByteBuffer.allocateDirect(WorldLayoutData.CUBE_COLORS.length * 4);
+        bbColors.order(ByteOrder.nativeOrder());
+        cubeColors = bbColors.asFloatBuffer();
+        cubeColors.put(WorldLayoutData.CUBE_COLORS);
+        cubeColors.position(0);
+
+        ByteBuffer bbFoundColors =
+                ByteBuffer.allocateDirect(WorldLayoutData.CUBE_FOUND_COLORS.length * 4);
+        bbFoundColors.order(ByteOrder.nativeOrder());
+        cubeFoundColors = bbFoundColors.asFloatBuffer();
+        cubeFoundColors.put(WorldLayoutData.CUBE_FOUND_COLORS);
+        cubeFoundColors.position(0);
+
+        ByteBuffer bbNormals = ByteBuffer.allocateDirect(WorldLayoutData.CUBE_NORMALS.length * 4);
+        bbNormals.order(ByteOrder.nativeOrder());
+        cubeNormals = bbNormals.asFloatBuffer();
+        cubeNormals.put(WorldLayoutData.CUBE_NORMALS);
+        cubeNormals.position(0);
+
+        // Init Floor
+        ByteBuffer bbFloorVertices = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_COORDS.length * 4);
+        bbFloorVertices.order(ByteOrder.nativeOrder());
+        floorVertices = bbFloorVertices.asFloatBuffer();
+        floorVertices.put(WorldLayoutData.FLOOR_COORDS);
+        floorVertices.position(0);
+
+        ByteBuffer bbFloorNormals = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_NORMALS.length * 4);
+        bbFloorNormals.order(ByteOrder.nativeOrder());
+        floorNormals = bbFloorNormals.asFloatBuffer();
+        floorNormals.put(WorldLayoutData.FLOOR_NORMALS);
+        floorNormals.position(0);
+
+        ByteBuffer bbFloorColors = ByteBuffer.allocateDirect(WorldLayoutData.FLOOR_COLORS.length * 4);
+        bbFloorColors.order(ByteOrder.nativeOrder());
+        floorColors = bbFloorColors.asFloatBuffer();
+        floorColors.put(WorldLayoutData.FLOOR_COLORS);
+        floorColors.position(0);
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+        Log.i(TAG, "onRendererShutdown()");
+
+
+    }
+}
